3379f3f6015a5b806c86626d2d56c3fa903998ef
nghiado <nghiado@NghiaDo.fritz.box>
2025-11-23 15:37:02 +0100

    day31

---
+++ day31.py
@@ -0,0 +1,8 @@
+def your_name(first, last):
+    #  first = first.capitalize()
+    #  last = last.capitalize()
+    return first + " " + last
+
+
+full_name = your_name("Van Nghia", "Do")
+print(full_name)
